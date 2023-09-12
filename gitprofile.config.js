c97f9bd763e4c00fdbe334f76f5645f207e183c1
Rakesh Chowdary Yarlagadda <33839890+rakeshy1116@users.noreply.github.com>
2023-09-12 17:50:41 -0600

    Update gitprofile.config.js

--- gitprofile.config.js
+++ gitprofile.config.js
@@ -2,7 +2,7 @@
 
 const config = {
   github: {
-    username: 'arifszn', // Your GitHub org/user name. (Required)
+    username: 'rakeshy1116', // Your GitHub org/user name. (Required)
     sortBy: 'stars', // stars | updated
     limit: 10, // How many projects to display.
     exclude: {
